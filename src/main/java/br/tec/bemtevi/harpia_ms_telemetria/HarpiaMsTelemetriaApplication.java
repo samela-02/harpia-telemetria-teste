2457e4901b0321d4c0d491c20d5ab88f070b74e2
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-04-16 17:54:43 -0300

    first commit

---
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/HarpiaMsTelemetriaApplication.java
@@ -0,0 +1,11 @@
+package br.tec.bemtevi.harpia_ms_telemetria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarpiaMsTelemetriaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HarpiaMsTelemetriaApplication.class, args);
+	}
+}
