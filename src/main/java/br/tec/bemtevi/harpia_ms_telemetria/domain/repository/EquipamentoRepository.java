071d3920f99927349da2f45d2b32e96abe5d7b52
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-04-17 11:05:15 -0300

    Adiciona Equipamento

---
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/domain/repository/EquipamentoRepository.java
@@ -0,0 +1,7 @@
+package br.tec.bemtevi.harpia_ms_telemetria.domain.repository;
+
+import br.tec.bemtevi.harpia_ms_telemetria.domain.model.Equipamento;
+
+public interface EquipamentoRepository {
+    Equipamento save(Equipamento equipamento);
+}
---
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/output/repository/equipamento/EquipamentoRepositoryImpl.java
@@ -0,0 +1,20 @@
+package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.output.repository.equipamento;
+
+import br.tec.bemtevi.harpia_ms_telemetria.domain.model.Equipamento;
+import br.tec.bemtevi.harpia_ms_telemetria.domain.repository.EquipamentoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EquipamentoRepositoryImpl implements EquipamentoRepository {
+    private final EquipamentoRepositoryMongo equipamentoRepositoryMongo;
+
+    public EquipamentoRepositoryImpl(EquipamentoRepositoryMongo equipamentoRepositoryMongo) {
+        this.equipamentoRepositoryMongo = equipamentoRepositoryMongo;
+    }
+
+    @Override
+    public Equipamento save(Equipamento equipamento) {
+        equipamento = equipamentoRepositoryMongo.save(equipamento);
+        return equipamento;
+    }
+}
