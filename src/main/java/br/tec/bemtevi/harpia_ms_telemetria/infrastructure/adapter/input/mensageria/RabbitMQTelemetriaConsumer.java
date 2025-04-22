0f5ece14ccafec5ccf7b2b4c696d0ed75e8a3693
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-04-22 09:08:41 -0300

    Refatora TelemetriaObserver
    A interface genérica Observer foi adicionada. Agora, o TelemetriaObserverImpl, que foi renomeado para TelemetriaObserver, implementa a interface Observer. Para que o consumidor importe o bean correto, o uso do Qualifier é necessário

--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/input/mensageria/RabbitMQTelemetriaConsumer.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/input/mensageria/RabbitMQTelemetriaConsumer.java
@@ -1,20 +1,21 @@
 package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.input.mensageria;
 
-import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.observer.TelemetriaObserver;
+import br.tec.bemtevi.harpia_ms_telemetria.domain.observer.Observer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQTelemetriaConsumer {
-    private final TelemetriaObserver telemetriaObserver;
+    private final Observer observer;
 
-    public RabbitMQTelemetriaConsumer(TelemetriaObserver telemetriaObserver) {
-        this.telemetriaObserver = telemetriaObserver;
+    public RabbitMQTelemetriaConsumer(@Qualifier(value = "TelemetriaObserver") Observer observer) {
+        this.observer = observer;
     }
 
     @RabbitListener(queues = "${rabbitmq.ms.queue.name}")
     public void consume(@Payload byte[] mensagem) {
-        telemetriaObserver.onEvent(mensagem);
+        observer.onEvent(mensagem);
     }
 }
--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/observer/TelemetriaObserver.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/observer/TelemetriaObserver.java
@@ -1,5 +1,33 @@
 package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.observer;
 
-public interface TelemetriaObserver {
-    void onEvent(byte[] mensagem);
+import br.tec.bemtevi.harpia_ms_telemetria.application.usecase.ProcessarTelemetriaUseCase;
+import br.tec.bemtevi.harpia_ms_telemetria.domain.model.Sensors;
+import br.tec.bemtevi.harpia_ms_telemetria.domain.observer.Observer;
+import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.input.dto.HarpiaTelemetryMessage;
+import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.anticorruptionlayer.HarpiaAntiCorruptionLayer;
+import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.facade.SerializationFacade;
+import org.springframework.stereotype.Component;
+
+@Component(value = "TelemetriaObserver")
+public class TelemetriaObserver implements Observer {
+    private final SerializationFacade serializationFacade;
+    private final HarpiaAntiCorruptionLayer harpiaAntiCorruptionLayer;
+    private final ProcessarTelemetriaUseCase processarTelemetriaUseCase;
+
+    public TelemetriaObserver(SerializationFacade serializationFacade,
+                              HarpiaAntiCorruptionLayer harpiaAntiCorruptionLayer,
+                              ProcessarTelemetriaUseCase processarTelemetriaUseCase) {
+        this.serializationFacade = serializationFacade;
+        this.harpiaAntiCorruptionLayer = harpiaAntiCorruptionLayer;
+        this.processarTelemetriaUseCase = processarTelemetriaUseCase;
+    }
+
+    @Override
+    public void onEvent(Object object) {
+        byte[] mensagem = (byte[]) object;
+        HarpiaTelemetryMessage harpiaTelemetryMessage = serializationFacade
+                .fromSnakeCaseBytes(mensagem, HarpiaTelemetryMessage.class);
+        Sensors sensors = harpiaAntiCorruptionLayer.fromHarpiaTelemetryMessage(harpiaTelemetryMessage);
+        processarTelemetriaUseCase.execute(sensors);
+    }
 }
