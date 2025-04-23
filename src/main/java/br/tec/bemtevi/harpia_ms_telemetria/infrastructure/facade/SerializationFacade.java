e01c37cf6556df6aed7e8b737c5ac6bb0cbfc0df
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-04-23 09:02:48 -0300

    Adiciona métodos para lidar com camel case no SerializationFacade

--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/facade/SerializationFacade.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/facade/SerializationFacade.java
@@ -1,5 +1,6 @@
 package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.facade;
 
 public interface SerializationFacade {
+    <T> T fromCamelCaseString(String object, Class<T> classType);
     <T> T fromSnakeCaseBytes(byte[] bytes, Class<T> classType);
 }
--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/facade/impl/JacksonSerializationFacade.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/facade/impl/JacksonSerializationFacade.java
@@ -1,10 +1,8 @@
 package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.facade.impl;
 
 import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.facade.SerializationFacade;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +10,31 @@ import java.io.IOException;
 
 @Component
 public class JacksonSerializationFacade implements SerializationFacade {
+    private final ObjectMapper objectMapperCamelCase;
     private final ObjectMapper objectMapperSnakeCase;
 
     public JacksonSerializationFacade() {
         JavaTimeModule javaTimeModule = new JavaTimeModule();
+        objectMapperCamelCase = new ObjectMapper();
+        objectMapperCamelCase.setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE);
+        objectMapperCamelCase.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapperCamelCase.registerModule(javaTimeModule);
+        objectMapperCamelCase.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         objectMapperSnakeCase = new ObjectMapper();
         objectMapperSnakeCase.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         objectMapperSnakeCase.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapperSnakeCase.registerModule(javaTimeModule);
         objectMapperSnakeCase.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    @Override
+    public <T> T fromCamelCaseString(String object, Class<T> classType) {
+        try {
+            return objectMapperCamelCase.readValue(object, classType);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Não foi possível converter a string em objeto.", e);
+        }
     }
 
     @Override
