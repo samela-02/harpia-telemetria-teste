ee55d276244d9aec71b89b761e8159516ade66b8
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-04-29 11:50:39 -0300

    Troca a classe pai que o GPSRepositoryMongo herda

--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/output/repository/lte/LTERepositoryMongo.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/output/repository/lte/LTERepositoryMongo.java
@@ -1,9 +1,9 @@
 package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.output.repository.lte;
 
 import br.tec.bemtevi.harpia_ms_telemetria.domain.model.LTE;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface LTERepositoryMongo extends CrudRepository<LTE, String> {
+interface LTERepositoryMongo extends MongoRepository<LTE, String> {
 }
