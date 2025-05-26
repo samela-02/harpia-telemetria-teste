2eb8a66982fbc30490260028837153b63a885469
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-05-26 12:14:14 -0300

    feat: adiciona ProcessarDispositivoUseCase

---
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/adapter/output/repository/dispositivo/DispositivoRepositoryImpl.java
@@ -0,0 +1,11 @@
+package br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.output.repository.dispositivo;
+
+import br.tec.bemtevi.harpia_ms_telemetria.domain.model.dispositivo.Dispositivo;
+import br.tec.bemtevi.harpia_ms_telemetria.domain.repository.DispositivoRepository;
+
+public class DispositivoRepositoryImpl implements DispositivoRepository {
+    @Override
+    public void save(Dispositivo dispositivo) {
+        throw new UnsupportedOperationException();
+    }
+}
