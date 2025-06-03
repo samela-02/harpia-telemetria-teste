92cbd0ba3e0cdb3be68a79d35bc48c53e4bff280
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-06-03 12:39:33 -0300

    feat: faz o fallback no TelemetriaObserver
    Agora, a cada erro, ou o microsserviço tentará se desligar ou irá fazer o fallback. Isso evitará chamadas ao rabbitmq desnecessariamente

--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/observer/TelemetriaObserver.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/infrastructure/observer/TelemetriaObserver.java
@@ -7,10 +7,12 @@ import br.tec.bemtevi.harpia_ms_telemetria.domain.model.Mensagem;
 import br.tec.bemtevi.harpia_ms_telemetria.domain.observer.Observer;
 import br.tec.bemtevi.harpia_ms_telemetria.domain.service.EquipamentoService;
 import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.input.dto.harpia.HarpiaTelemetryMessage;
+import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.output.dto.FallbackDto;
+import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.adapter.output.gateway.FallbackGateway;
 import br.tec.bemtevi.harpia_ms_telemetria.infrastructure.anticorruptionlayer.HarpiaAntiCorruptionLayer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component(value = "TelemetriaObserver")
 public class TelemetriaObserver implements Observer {
@@ -19,41 +21,69 @@ public class TelemetriaObserver implements Observer {
     private final HarpiaAntiCorruptionLayer harpiaAntiCorruptionLayer;
     private final EquipamentoService equipamentoService;
     private final ProcessarMensagemUseCase processarMensagemUseCase;
+    private final FallbackGateway fallbackGateway;
 
     public TelemetriaObserver(LoggerFacade loggerFacade,
                               SerializationFacade serializationFacade,
                               HarpiaAntiCorruptionLayer harpiaAntiCorruptionLayer,
                               EquipamentoService equipamentoService,
-                              ProcessarMensagemUseCase processarMensagemUseCase) {
+                              ProcessarMensagemUseCase processarMensagemUseCase,
+                              FallbackGateway fallbackGateway) {
         this.loggerFacade = loggerFacade;
         this.serializationFacade = serializationFacade;
         this.harpiaAntiCorruptionLayer = harpiaAntiCorruptionLayer;
         this.equipamentoService = equipamentoService;
         this.processarMensagemUseCase = processarMensagemUseCase;
+        this.fallbackGateway = fallbackGateway;
     }
 
     @Override
     public void onEvent(Object object) {
         loggerFacade.info("Notificação de mensagem recebida.");
+        Mensagem mensagem;
         try {
-            processarMensagem((byte[]) object);
+            HarpiaTelemetryMessage harpiaTelemetryMessage = converterBytesEmHarpiaTelemetryMessage((byte[]) object);
+            atualizarUltimaComunicacaoEquipamento(harpiaTelemetryMessage.getSerial());
+            mensagem = converterHarpiaTelemetryMessageEmMensagem(harpiaTelemetryMessage);
         } catch (Exception e) {
-            processarFallback((byte[]) object);
+            loggerFacade.warn(String.format("Erro ao atualizar a data de última comunicação: %s.", e.getMessage()));
+            // TODO chamar shutdown aqui
+            throw new RuntimeException(e);
+        }
+
+        try {
+            processarMensagem(mensagem);
+        } catch (Exception e) {
+            processarFallback(mensagem, e);
         }
     }
 
-    private void processarMensagem(byte[] object) {
-        HarpiaTelemetryMessage harpiaTelemetryMessage = serializationFacade
-                .fromSnakeCaseBytes(object, HarpiaTelemetryMessage.class);
-        Mensagem mensagem = harpiaAntiCorruptionLayer.fromHarpiaTelemetryMessage(harpiaTelemetryMessage);
+    private Mensagem converterHarpiaTelemetryMessageEmMensagem(HarpiaTelemetryMessage harpiaTelemetryMessage) {
+        return harpiaAntiCorruptionLayer.fromHarpiaTelemetryMessage(harpiaTelemetryMessage);
+    }
+
+    private HarpiaTelemetryMessage converterBytesEmHarpiaTelemetryMessage(byte[] mensagem) {
+        return serializationFacade.fromSnakeCaseBytes(mensagem, HarpiaTelemetryMessage.class);
+    }
+
+    private void atualizarUltimaComunicacaoEquipamento(String idEquipamento) {
+        equipamentoService.atualizarDtUltimaAtualizacao(idEquipamento);
+    }
+
+    private void processarMensagem(Mensagem mensagem) {
         processarMensagemUseCase.execute(mensagem);
-        equipamentoService.atualizarDtUltimaAtualizacao(harpiaTelemetryMessage.getSerial());
         loggerFacade.info("Mensagem processada com sucesso.");
     }
 
-    private void processarFallback(byte[] object) {
+    private void processarFallback(Mensagem mensagem, Exception e) {
         loggerFacade.warn("Erro ao processar mensagem.");
         loggerFacade.info("Iniciando fallback");
-        // TODO chamar fallback aqui
+        fallbackGateway.enviarFallback(new FallbackDto("Microsserviço de detecção",
+                LocalDateTime.now(),
+                e.getMessage(),
+                e.getCause(),
+                mensagem)
+        );
+        loggerFacade.info("Fallback enviado com sucesso.");
     }
 }
