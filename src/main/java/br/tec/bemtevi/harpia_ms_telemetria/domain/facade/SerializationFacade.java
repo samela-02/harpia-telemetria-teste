8aeddf8f8582e4cffc22fb53664a54373891f1a1
Igor Tauhan <igor.tauhan@tivic.com.br>
2025-06-03 17:59:49 -0300

    feat: adiciona SerializationFacade.asSnakeCaseBytes

--- src/main/java/br/tec/bemtevi/harpia_ms_telemetria/domain/facade/SerializationFacade.java
+++ src/main/java/br/tec/bemtevi/harpia_ms_telemetria/domain/facade/SerializationFacade.java
@@ -2,6 +2,7 @@ package br.tec.bemtevi.harpia_ms_telemetria.domain.facade;
 
 public interface SerializationFacade {
     String asSnakeCaseString(Object object);
+    byte[] asSnakeCaseBytes(Object object);
     <T> T fromCamelCaseString(String object, Class<T> classType);
     <T, S> T fromCamelCaseStringParameterized(String object, Class<T> classType, Class<S> parameterizedClass);
     <T> T fromSnakeCaseBytes(byte[] bytes, Class<T> classType);
